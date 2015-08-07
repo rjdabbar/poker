8db095c7fa83016d3b6b9b55269987a81302ff0f
tyler + rj <tyler+rj@gmail.com>
2015-08-07 11:38:07 -0400

    Add stock_picker

--- lib/array_exercises.rb
+++ lib/array_exercises.rb
@@ -19,4 +19,37 @@ class Array
     end
     new_array
   end
+
+  def my_transpose
+    temp_arr = self.dup
+    trans_arr = []
+
+    temp_arr.size.times do
+      col = []
+
+      temp_arr.each do |row|
+        col << row.shift
+      end
+
+      trans_arr << col
+    end
+
+    trans_arr
+  end
+end
+
+def stock_picker(array)
+  max = 0
+  days = [0,0]
+
+  (0...array.count).each do |buy_idx|
+    ((buy_idx)...array.count).each do |sell_idx|
+      if array[sell_idx] - array[buy_idx] > max
+        max = array[sell_idx] - array[buy_idx]
+        days[0], days[1] = buy_idx, sell_idx
+      end
+    end
+  end
+
+  return days
 end
--- spec/array_exercises_spec.rb
+++ spec/array_exercises_spec.rb
@@ -23,6 +23,7 @@ describe Array do
 
   describe "#two_sum" do
     let(:arr) { [-1, 0, 2, -2, 1] }
+
     it "should return an array" do
       expect(arr.two_sum).to be_kind_of(Array)
     end
@@ -49,16 +50,48 @@ describe Array do
 
   end
 
-  describe "#my_trasnpose" do
-    let(:arr) = { [[0, 1, 2], [3, 4, 5], [6, 7, 8]] }
+  describe "#my_transpose" do
+    let(:arr1) { [[0, 1, 2], [3, 4, 5], [6, 7, 8]] }
+    let(:arr2) { [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]]}
+    let(:trans_arr1) { [[0, 3, 6], [1, 4, 7], [2, 5, 8]] }
+    let(:trans_arr2) { [[0, 4, 8, 12], [1, 5, 9, 13], [2, 6, 10, 14], [3, 7, 11, 15]]}
 
     it "should return an array" do
-      expect(arr.two_sum).to be_kind_of(Array)
+      expect(arr1.my_transpose).to be_kind_of(Array)
     end
 
-    it "should not modify the original array"
+    it "should not modify the original array" do
+      expect(arr1.my_transpose).not_to eq(arr1)
+    end
 
-    it "should transpose square matricies of any size"
+    it "should transpose square matricies of any size" do
+      expect(arr1.my_transpose).to eq(trans_arr1)
+      expect(arr2.my_transpose).to eq(trans_arr2)
+    end
 
+    it "should not use the built in Array#transpose method" do
+      expect(arr1).to_not receive(:transpose)
+    end
+
+  end
+
+  describe "#stock_picker" do
+    let(:arr) { [12, 14, 17, 20, 45, 32, 28, 26, 32] }
+
+    it "should return an array" do
+      expect(stock_picker(arr)).to be_kind_of(Array)
+    end
+
+    it "should return an array of two elements" do
+      expect(stock_picker(arr).count).to eq(2)
+    end
+
+    it "should return a sorted pair" do
+      expect(stock_picker(arr)[0]).to be < stock_picker(arr)[1]
+    end
+
+    it "should return the most profitable pair of days" do
+      expect(stock_picker(arr)).to eq([0, 4])
+    end
   end
 end
